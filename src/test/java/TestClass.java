60c2981b5a8ef3f6a8d148b0bd7f3a6c39aa6828
Ekaterina Zarubina <zarubina.e4@wb.ru>
2024-07-13 21:12:03 +0700

    fix: selenium and selenide dependencies

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -4,17 +4,29 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestClass {
-    WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "D:/distr/Autotesting/webdrivers/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
+    @Test
+    public void testOpenPage() {
+        //Открыть страницу
+        driver.get("https://www.google.com");
+        //Проверить заголовок страницы
+        String expectedTitle = "Example Domain";
+        String actualTitle = driver.getTitle();
+        assertEquals(expectedTitle, actualTitle);
+    }
 
     @After
     public void tearDown() {
-        // Закройте браузер
         if (driver != null) {
             driver.quit();
         }
